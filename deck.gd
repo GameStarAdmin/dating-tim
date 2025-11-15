dbde9e4e96cbd01700887a44460b4f20fb9d786f
Lorenzo <lorenzo.padrini@epfl.ch>
2025-11-15 17:13:06 +0100

    moved cards into deck

--- deck.gd
+++ deck.gd
@@ -7,8 +7,8 @@ var card_types = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in range(DECK_SIZE):
-		deck.append(card_types.pick_random())
-
+		deck.append(get_node("BaseCard"))
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- nodes/base_card.gd
+++ nodes/base_card.gd
@@ -1,44 +1,47 @@
 extends Sprite2D
 
-class Stats:
-	var toxic = 0
-	var manipulation = 0
-	var beauf = 0
-	var nerd = 0
-	var clown = 0
-	var simp = 0
-	var riche = 0
-	var wholesome = 0
-	var sympa = 0
+var stats = {
+  "toxique" : 0,
+  "beauf" : 0,
+  "nerd" : 0,
+  "clown" : 0,
+  "simp" : 0,
+  "riche" : 0,
+  "sympa" : 0,
+  "cool" : 0,
+  "kinky" : 0,
+  "cute" : 0,
+  "kawaii" : 0,
+  "cringe" : 0,
+}
 
-var stats = Stats.new()
-var requirements = Stats.new()
+var requirements = {
+  "toxique" : 0,
+  "beauf" : 0,
+  "nerd" : 0,
+  "clown" : 0,
+  "simp" : 0,
+  "riche" : 0,
+  "sympa" : 0,
+  "cool" : 0,
+  "kinky" : 0,
+  "cute" : 0,
+  "kawaii" : 0,
+  "cringe" : 0,
+}
+
 
 func requires(out_stats)-> bool:
 	return stats <= out_stats
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func new(toxic, manipulation, beauf, nerd, clown, simp, riche, wholesome, sympa, image) -> void:
-	stats.toxic = toxic
-	stats.manipulation = manipulation
-	stats.beauf = beauf
-	stats.nerd = nerd
-	stats.clown = clown
-	stats.simp = simp
-	stats.riche = riche
-	stats.wholesome = wholesome
-	stats.sympa = sympa
-	stats.texture = image
 	
-
 
 func get_stats() -> Stats:
 	return stats
