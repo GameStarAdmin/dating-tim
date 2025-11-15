9861f5ee925896955f6caa48724dc7d5a4f9522f
Leander Cretegny <leander.cretegny@epfl.ch>
2025-11-15 23:48:49 +0100

    add camera and target script modification

--- target.gd
+++ target.gd
@@ -1,5 +1,6 @@
 class_name Target
 extends Node
+var names = ["Tim"]
 
 #Stats (multipliers)
 var mults = {
@@ -17,8 +18,26 @@ var mults = {
 	"cringe" : 0,
 }
 
+var target_name = ""
+
+func _ready() -> void:
+	target_name = names[randi_range(0, names.size())]
+	$Sprite3D.texture = preload("res://images/tim2.png")
+
 func DamageCompute(card : BaseCard):
 	var final_rizz = 0
 	for stat in mults:
 		final_rizz += mults[stat] * card.stats[stat]
+	
+	if final_rizz < -12:
+		$Sprite3D.texture = load("res://images/tim0.png")
+	elif final_rizz < -6:
+		$Sprite3D.texture = load("res://images/tim1.png")
+	elif final_rizz < 0:
+		$Sprite3D.texture = load("res://images/tim2.png")
+	elif final_rizz < 6:
+		$Sprite3D.texture = load("res://images/tim3.png")
+	else:
+		$Sprite3D.texture = load("res://images/tim4.png")
+		
 	return final_rizz
