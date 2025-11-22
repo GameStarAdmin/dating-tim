31091ba966dcf3828f7e138cf1915717d68811c7
HackFight <hi.hugo.cornu@hotmail.com>
2025-11-22 16:39:38 +0100

    Handles end of deck

--- global_vars.gd
+++ global_vars.gd
@@ -1,3 +1,5 @@
 extends Node
 
+var win_by_max_rizz = false
 var winner = 0
+var rizz_amount = 0
--- win_screen.gd
+++ win_screen.gd
@@ -3,4 +3,7 @@ extends Node2D
 @export var label : Label
 
 func _ready() -> void:
-	label.text = "Player" + str(global_vars.winner) + " is RIZZMAXING!!!"
+	if(global_vars.win_by_max_rizz):
+		label.text = "Player" + str(global_vars.winner) + " is RIZZMAXING!!!"
+	else:
+		label.text = "Player" + str(global_vars.winner) + " is the least bad of you two with " + str(int(round(global_vars.rizz_amount))) + " rizz."
