abb04e40283cdf7d9250339f256682916c7357e7
HackFight <hi.hugo.cornu@hotmail.com>
2025-11-22 18:15:28 +0100

    ALL MIGHT TWT

--- win_screen.gd
+++ win_screen.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 @export var label : Label
+@export var name_label : Label
 
 func _ready() -> void:
 	if(global_vars.win_by_max_rizz):
 		label.text = "Player" + str(global_vars.winner) + " is RIZZMAXING!!!"
 	else:
 		label.text = "Player" + str(global_vars.winner) + " is the least bad of you two with " + str(int(round(global_vars.rizz_amount))) + " rizz."
+		
+	name_label.text = "Player" + str(global_vars.winner)
